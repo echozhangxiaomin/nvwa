73908c05513e308cc8ad9c991c2182864fc7752a
echozhangxiaomin <86400430+echozhangxiaomin@users.noreply.github.com>
2023-05-05 17:19:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
 
+import streamlit as st
+
+def main():
+  st.title("欢迎使用 Streamlit")
+  st.header("这是一个简单的示例程序")
+
+  if st.button("点击这里"):
+    st.write("你好，世界！")
+
+if __name__ == "__main__":
+  main()
